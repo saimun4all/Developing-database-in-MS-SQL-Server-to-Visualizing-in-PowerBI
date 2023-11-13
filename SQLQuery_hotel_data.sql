07d38f8b127a41f693f8fd4ff3de00fc48d09340
Saimun Hassan <123021229+saimun4all@users.noreply.github.com>
2023-11-12 19:09:28 -0500

    Present all tables as a Unified Dataset using SQL query

--- SQLQuery_hotel_data.sql
+++ SQLQuery_hotel_data.sql
@@ -1,2 +1,7 @@
-SELECT * FROM 
-[Hotel Project].dbo.['2018$']
+-----------* Show all tables as a Unified Dataset *------------
+
+SELECT * FROM dbo.['2018$']
+UNION
+SELECT * FROM dbo.['2019$']
+UNION
+SELECT * FROM dbo.['2020$']
