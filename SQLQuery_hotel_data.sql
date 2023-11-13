c41536595f398fdf122ea3ca83165b0caa6a2d5e
Saimun Hassan <123021229+saimun4all@users.noreply.github.com>
2023-11-12 19:38:00 -0500

    Find out Total Revenue Breakdown Year-wise

--- SQLQuery_hotel_data.sql
+++ SQLQuery_hotel_data.sql
@@ -5,3 +5,18 @@ UNION
 SELECT * FROM dbo.['2019$']
 UNION
 SELECT * FROM dbo.['2020$']
+
+-----------* Find out Total Revenue Breakdown Year-wise *------------
+WITH hotels AS (
+	SELECT * FROM dbo.['2018$']
+UNION
+SELECT * FROM dbo.['2019$']
+UNION
+SELECT * FROM dbo.['2020$'])
+
+SELECT
+arrival_date_year,
+SUM((stays_in_weekend_nights + stays_in_week_nights)*adr)
+AS Revenue
+FROM hotels
+GROUP BY arrival_date_year
