fd8b02098d6bf8ff075b1bf0a3371312abaecd6b
Saimun Hassan <123021229+saimun4all@users.noreply.github.com>
2023-11-13 19:34:08 -0500

    Used left join to find market segment and type of meals

--- SQLQuery_hotel_data.sql
+++ SQLQuery_hotel_data.sql
@@ -17,7 +17,22 @@ SELECT * FROM dbo.['2020$'])
 SELECT
 arrival_date_year,
 hotel,
-SUM((stays_in_weekend_nights + stays_in_week_nights)*adr)
+ROUND(SUM((stays_in_weekend_nights + stays_in_week_nights)*adr),2)
 AS Revenue
 FROM hotels
 GROUP BY arrival_date_year, hotel
+
+
+-----------* Find out Total Revenue Breakdown by Hotel type and Year-wise *------------
+WITH hotels AS (
+	SELECT * FROM dbo.['2018$']
+UNION
+SELECT * FROM dbo.['2019$']
+UNION
+SELECT * FROM dbo.['2020$'])
+SELECT * FROM 
+ hotels
+ LEFT JOIN dbo.market_segment$
+ ON hotels.market_segment = dbo.market_segment$.market_segment
+ LEFT JOIN dbo.meal_cost$
+ ON meal_cost$.meal = hotels.meal
